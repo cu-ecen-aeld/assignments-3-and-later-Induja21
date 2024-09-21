b1f40d4782705be72110c3bb30267ee3d60da45e
Induja Narayanan <n.induja@gmail.com>
2024-09-21 12:37:53 -0600

    Add: Threading component
    As part of this commit
    A function to create thread and execute below operations is done
    1) waits for input ms time
    2) acquires mutex lock
    3) waits for input ms time
    4) Release mutex lock
    5) return thread handler to be freed
    
    Signed-off-by: Induja Narayanan <n.induja@gmail.com>

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,23 +2,54 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
-#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+#define DEBUG_LOG(msg, ...) printf("threading DEBUG: " msg "\n", ##__VA_ARGS__)
+// #define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+#define ERROR_LOG(msg, ...) printf("threading ERROR: " msg "\n", ##__VA_ARGS__)
 
-void* threadfunc(void* thread_param)
+#define SUCCESS 0
+void *threadfunc(void *thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    // struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int ret_status = -1;
+    // Wait for thread_func_args->wait_to_obtain_ms
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    ret_status = pthread_mutex_lock (thread_func_args->mutex);
+    if(ret_status != SUCCESS)
+    {
+        thread_func_args->thread_complete_success = false;
+        ERROR_LOG("pthread acquiring mutex failed. Error reason: %s",strerror(ret_status));
+    }
+    else
+    {
+        usleep(thread_func_args->wait_to_release_ms * 1000);
+        ret_status = pthread_mutex_unlock (thread_func_args->mutex);
+        if(ret_status != SUCCESS)
+        {
+            thread_func_args->thread_complete_success = false;
+            ERROR_LOG("pthread releasing mutex failed. Error reason: %s",strerror(ret_status));
+            
+        }
+        else
+        {
+            thread_func_args->thread_complete_success = true;
+            DEBUG_LOG("pthread execution is successful");
+        }
+        
+    }
+
+    
     return thread_param;
 }
 
-
-bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
@@ -28,6 +59,31 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    bool thread_creation_success = false;
+    int pthread_create_status;
+    struct thread_data *thread_data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if(thread_data==NULL)
+    {
+        return thread_creation_success;
+    }
 
+    thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data->wait_to_release_ms = wait_to_release_ms;
+    thread_data->mutex = mutex;
+    pthread_create_status = pthread_create(thread, NULL, threadfunc, thread_data);
+    if (pthread_create_status== SUCCESS)
+    {
+        DEBUG_LOG("pthread creation is successful");
+        thread_creation_success = true;
+    }
+    else
+    {
+        thread_creation_success = false;
+        free(thread_data);
+        ERROR_LOG("pthread creation failed. Error reason: %s",strerror(pthread_create_status));
+        
+    }
+
+
+    return thread_creation_success;
+}
